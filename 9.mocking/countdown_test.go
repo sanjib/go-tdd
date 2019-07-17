1269ba5eab9267e7f01350a119a5c98cfc77299c
Sanjib Ahmad <sanjib@sanjib.org>
2019-07-17 12:16:21 +0600

    ensure correct sequence of sleep and print in tests for Countdown

--- 9.mocking/countdown_test.go
+++ 9.mocking/countdown_test.go
@@ -2,8 +2,27 @@ package mocking
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
+
+const (
+	write = "write"
+	sleep = "sleep"
+)
+
+type CountdownOperationsSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
 
 type SpySleeper struct {
 	Calls int
@@ -32,4 +51,24 @@ Go!`
 	if spySleeper.Calls != 4 {
 		t.Errorf("not enough calls to sleeper, got %d, want %d", spySleeper.Calls, 4)
 	}
+
+	t.Run(`sleep before every print`, func(t *testing.T) {
+		spySleepPrinter := CountdownOperationsSpy{}
+		Countdown(&spySleepPrinter, &spySleepPrinter)
+
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted %v, got %v", want, spySleepPrinter.Calls)
+		}
+	})
 }
