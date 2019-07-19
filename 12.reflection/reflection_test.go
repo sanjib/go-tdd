b25b264fc02486970ee62fc804ebbebbcaa92503
Sanjib Ahmad <sanjib@sanjib.org>
2019-07-19 10:03:10 +0600

    add table driven tests and variable interface length in walk

--- 12.reflection/reflection_test.go
+++ 12.reflection/reflection_test.go
@@ -1,8 +1,36 @@
 package reflection
 
 import (
+	"reflect"
 	"testing"
 )
+
+func TestWalkTableDriven(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{"struct with one string field", struct{ Name string }{"Sanjib"}, []string{"Sanjib"}},
+		{"struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Sanjib", "Chittagong"},
+			[]string{"Sanjib", "Chittagong"}},
+	}
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
 
 func TestWalk(t *testing.T) {
 	expected := "Sanjib"
